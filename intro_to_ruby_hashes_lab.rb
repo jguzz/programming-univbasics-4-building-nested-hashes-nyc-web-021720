dbabe78ef099ec1c417f76d43674fa1ec27f10c0
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-03 00:16:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,7 +61,7 @@ def monopoly_with_fourth_tier
 	    },
 	    :names => {
 	      :reading_railroad => {
-	        "mortgage_value" => "$q00"
+	        "mortgage_value" => "$100"
 	      },
 	      :pennsylvania_railroad => {
 	        "mortgage_value" => "$"
